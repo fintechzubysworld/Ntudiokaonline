b5cc9ed3a19c4736755ab87f2f56eba284ded23d
Zuby's World Logistics <fintech.zubysworld@gmail.com>
2026-02-13 19:44:04 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'ntudioka-cache-v1';
+const urlsToCache = [
+  'youth.html',
+  'manifest.json',
+  'https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://apis.google.com/js/api.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
